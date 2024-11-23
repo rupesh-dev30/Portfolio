880947517c6d26998061a024a114d0dd36a412dc
Rupesh Kumar <125800432+rupesh-dev30@users.noreply.github.com>
2024-11-24 04:00:49 +0530

    Update Resume.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -97,7 +97,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1bCJuKZxCRDiD5R_9AU0LsPXh_Uui_HcT/view"
+          href="https://drive.google.com/file/d/1mLZJrUBXOgkNWuW0B_ubaOMGzThWHE6e/view"
           target="_blank"
           download
         >
